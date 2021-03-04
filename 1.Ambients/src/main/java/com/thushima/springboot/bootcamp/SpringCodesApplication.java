5a66f97f595aed9dae3921dfc041338f0313ce40
Elismar <elismarsilva.5@outlook.com>
2021-03-04 20:33:59 -0300

    :tada: init

---
+++ 1.Ambients/src/main/java/com/thushima/springboot/bootcamp/SpringCodesApplication.java
@@ -0,0 +1,13 @@
+package com.thushima.springboot.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCodesApplication.class, args);
+	}
+
+}
---
+++ 1.Ambients/src/test/java/com/thushima/springboot/bootcamp/SpringCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thushima.springboot.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
